2fe6b29ff8bf5f1940b36462426e9d3e23235c0e
manojkumarsmks <manojkumarsmks@gmail.com>
2018-05-08 23:11:18 -0600

    Bubble sort comments

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,3 +1,5 @@
+import javax.print.DocFlavor.INPUT_STREAM;
+
 // Bubble sort - Swapping algorithm
 public class BubbleSort {
 	int[] inputArray;
@@ -8,9 +10,9 @@ public class BubbleSort {
 	}
 	
 	public void BubbleSortAlgo() {
+		// Swap for every element at i > every element at j 
 		for(int i =0; i<inputArray.length-1; i++) {
 			for(int j = i+1; j<inputArray.length;j++) {
-				
 				if(inputArray[i] > inputArray[j]) {
 					int temp = inputArray[i];
 					inputArray[i] = inputArray[j];
